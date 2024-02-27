0511b9d22bfb4305540621d91455a78fdd59f9ea
MrCedie <cedricklangcap@gmail.com>
2024-02-27 13:54:15 +0800

    update env variable name

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.REACT_APP_VERCEL_API_KEY,
+  authDomain: process.env.REACT_APP_VERCEL_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_VERCEL_DATABASE_URL,
+  projectId: process.env.REACT_APP_VERCEL_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_VERCEL_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_VERCEL_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_VERCEL_APP_ID,
 };
 
 // Initialize Firebase
