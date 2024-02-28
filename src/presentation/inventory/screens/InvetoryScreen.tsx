f0ddc8510f9926b217135a3bed03f575653a0f1d
MrCedie <cedricklangcap@gmail.com>
2024-02-28 18:23:03 +0800

    remove pagination

--- src/presentation/inventory/screens/InvetoryScreen.tsx
+++ src/presentation/inventory/screens/InvetoryScreen.tsx
@@ -148,6 +148,7 @@ const InvetoryScreen = () => {
         columns={tableInventoryColumn}
         dataSource={table}
         loading={loading}
+        pagination={false}
       />
       <Modal
         title=""
